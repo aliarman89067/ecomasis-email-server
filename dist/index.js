4dcc55519ac4789c674fc43f4a454d544c868519
Ali Arman <ali@gmail.com>
2025-07-18 03:18:02 +0500

    First commit

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+require("dotenv/config");
+const lead_controller_1 = require("./controllers/lead-controller");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: process.env.CLIENT_ENDPOINT,
+}));
+app.post("/generate-lead", lead_controller_1.generateLead);
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+});
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import nodemailer from "nodemailer";
+import { generateLead } from "./controllers/lead-controller";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_ENDPOINT,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/generate-lead", generateLead);
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
